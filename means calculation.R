ec7e64d8ab48f62adb2bc009d2beaf86150bdfe6
Xin0926 <55975472+Xin0926@users.noreply.github.com>
2019-10-02 02:58:34 +0800

    Update means calculation.R

--- means calculation.R
+++ means calculation.R
@@ -19,8 +19,8 @@ Month <- c(
 
 Year <- c( rep(c(2005,2006,2007), each=8760),rep(2008,each=8784), rep(c(2009,2010,2011),each=8760), rep(2012,each=8784),rep(c(2013,2014,2015),each=8760),rep(2016,each=8784),  rep(c(2017,2018),each=8760))
 
-library(readr)
-tower.flux <- read_csv("https://github.com/Xin0926/means-calculation/raw/master/tower.flux.csv.zip")
+#library(readr)
+tower.flux <- read.csv("your own path/tower.flux.csv", sep = "", fill = TRUE, na.strings = "NA")
 
 heat <- NULL
 for(i in seq(1,736128,by=6)){
